dab3051e43718c1f8577cba11e0cfffb51400561
Nick Koutrelakos <nick.koutrelakos@gmail.com>
2017-05-27 12:12:54 -0700

    cleaned stuff up

--- src/sample-fishes.js
+++ src/sample-fishes.js
@@ -1,4 +1,4 @@
-// This is just some sample data so you don't have to think of your own!
+// This is sample data to load as "store owner"
 module.exports = {
   fish1: {
     name: 'Pacific Halibut',
